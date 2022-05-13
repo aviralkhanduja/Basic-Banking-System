45dfeeeda186123474cd8c8c575ec163142a00a1
Aviral <akhanduja_be19@thapar.edu>
2022-05-13 15:57:42 +0530

    updated final for lab eval

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const passport=require('passport');
 const home_controller=require('../controllers/home_controller');
 const userrouter=require('./user');
+
 router.get('/',passport.restrictAccess,home_controller.login);
 router.get('/signup',passport.restrictAccess,home_controller.signup);
 router.post('/signup/adduser',passport.restrictAccess,home_controller.createUser);
